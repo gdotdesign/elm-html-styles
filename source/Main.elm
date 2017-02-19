59f69c0df85b157b380ec1c2ac4be6b1bd437487
Szikszai Gusztáv <guszti5@hotmail.com>
2017-02-19 09:04:00 +0100

    Implement style removal.

--- source/Main.elm
+++ source/Main.elm
@@ -1,38 +1,66 @@
 import StyledHtml
 
 import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
 import Html exposing (text)
+import Html.Lazy
+
 import Mouse
 
-type alias Model = Mouse.Position
+type alias Model =
+  { show : Bool }
 
-type Msg = Move Mouse.Position
+type Msg = Toggle
+  | Move Mouse.Position
 
 init : Model
 init =
-  { x = 0, y = 0 }
+  { show = False }
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg_ model =
   case msg_ of
-    Move pos -> (pos, Cmd.none)
+    Move pos ->
+      (model, Cmd.none)
+
+    Toggle ->
+      ({ model | show = not model.show }, Cmd.none)
 
 view : Model -> Html.Html Msg
 view model =
-  StyledHtml.node "div"
-    [ ("background", "red")
-    , ("position", "absolute")
-    , ("left", (toString model.x) ++ "px")
-    , ("top", (toString model.y) ++ "px")
-    ]
-    []
-    [text "hello"]
+  let
+    display =
+      if model.show then
+        "block"
+      else
+        "none"
+  in
+    StyledHtml.node "div"
+      [ ("background", "red")
+      , ("position", "absolute")
+      , ("padding", "20px")
+      , ("font-family", "sans")
+      ]
+      []
+      [ StyledHtml.node "span"
+        [("font-size", "20px")
+        ,("display", display) ]
+        []
+        [text "Blah"]
+      , StyledHtml.node "button"
+        [ ("background", "blue")
+        , ("border", "0")
+        , ("color", "#FFF")
+        ]
+        [ onClick Toggle ]
+        [ text "Toggle" ]
+      ]
 
 
 main =
   Html.program
     { init = (init, Cmd.none)
     , update = update
-    , view = view
+    , view = Html.Lazy.lazy view
     , subscriptions = \_ -> Mouse.moves Move
     }
--- source/Native/Styles.js
+++ source/Native/Styles.js
@@ -1,43 +1,37 @@
-var nextId = (function() {
-  var nextIndex = [0,0,0];
-  var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-  var num = chars.length;
-
-  return function() {
-    var a = nextIndex[0];
-    var b = nextIndex[1];
-    var c = nextIndex[2];
-    var id = chars[a] + chars[b] + chars[c];
-
-    a = ++a % num;
-
-    if (!a) {
-      b = ++b % num;
-
-      if (!b) {
-        c = ++c % num;
-      }
-    }
-    nextIndex = [a, b, c];
-    return id;
-  }
-}());
-window.nextId = nextId
-
 var _gdotdesign$elm_styled_html$Native_Styles = function() {
+  var currentID = 0
+
   var styleElement = document.createElement('style')
   document.head.appendChild(styleElement)
 
   var createRule = function(id) {
     styleElement.sheet.insertRule('.' + id + '{}', 0)
-    return styleElement.sheet.rules[0]
+    return styleElement.sheet.cssRules[0]
   }
 
-  Object.defineProperty(HTMLElement.prototype, 'styles', {
+  var removeRule = function(child){
+    if(child.__styleNode) {
+      child.__styleNode.parentStyleSheet.deleteRule(child.__styleNode)
+    }
+  }
+
+  Element.prototype.oldRemoveChild = Element.prototype.removeChild
+  Element.prototype.removeChild = function(child){
+    removeRule(child)
+    return this.oldRemoveChild(child)
+  }
+
+  Element.prototype.oldReplaceChild = Element.prototype.replaceChild
+  Element.prototype.replaceChild = function(newNode, child){
+    removeRule(child)
+    return this.oldReplaceChild(newNode, child)
+  }
+
+  Object.defineProperty(Element.prototype, 'styles', {
     set: function(values){
       if (!values.length) { return }
       if (!this.__styleNode) {
-        var id = 's-' + nextId()
+        var id = 's-' + (currentID++)
         this.classList.add(id)
         this.__styleNode = createRule(id)
       }
